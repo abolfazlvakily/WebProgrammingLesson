4fdc92ee776ceb12c1d9d6b6a02039623c5aecde
abolfazlvakily <abolfazlvakily@gmail.com>
2021-10-14 13:34:06 +0330

    fsd

--- t1/src/index.php
+++ t1/src/index.php
@@ -1,20 +1,71 @@
-<?php
-//These are the defined authentication environment in the db service
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v30.1.0/dist/font-face.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<h1>نام و نام خانوادگی: ابوالفضل وکیلی</h1>
+<h3>برنامه اي بنويسيد که طول و عرض مستطيلي را از کاربر گرفته و مساحت آن را محاسبه کند.</h3>
+<form action="result.php" method="post">
+ <ul>
+  <li>
+    <label for="length">طول</label>
+    <input type="number" id="length" name="length-var" value=0>
+  </li>
+  <li>
+    <label for="width">عرض</label>
+    <input type="number" id="width" name="width-var" value=0>
+  </li>
+  <li class="button" style="text-align: left;">
+  <button type="submit" name="result-q1" value=0>ارسال</button>
+  </li>
+ </ul>
+</form>
 
-// The MySQL service named in the docker-compose.yml.
-$host = 'test-project-mysql';
-// $host = 'db';
-// Database use name
-$user = 'vakily';
 
-//database user password
-$pass = 'CfhUfrq2PzaFTUVL';
+<h3>برنامه اي بنويسيد كه مساحت مثلثي را براي مقادير ورودي كاربر محاسبه كند.</h3>
+<form action="result.php" method="post">
+ <ul>
+  <li>
+    <label for="length">طول</label>
+    <input type="number" id="length" name="length-var" value=0>
+  </li>
+  <li>
+    <label for="width">عرض</label>
+    <input type="number" id="width" name="width-var" value=0>
+  </li>
+ </ul>
+</form>
 
-// check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
-}
-?>
+
+<h3>برنامهاي بنويسيد زوج يا فرد بودن عدد ورودي را تعيين كند.</h3>
+<form action="/" method="post">
+ <ul>
+  <li>
+    <label for="length">طول</label>
+    <input type="number" id="length" name="length-var" value=0>
+  </li>
+  <li>
+    <label for="width">عرض</label>
+    <input type="number" id="width" name="width-var" value=0>
+  </li>
+ </ul>
+</form>
+
+<h3>برنامهاي بنويسيد كه فاكتوريل عدد مورد نظر كاربر را حساب كند.</h3>
+<form action="/" method="post">
+ <ul>
+  <li>
+    <label for="length">طول</label>
+    <input type="number" id="length" name="length-var" value=0>
+  </li>
+  <li>
+    <label for="width">عرض</label>
+    <input type="number" id="width" name="width-var" value=0>
+  </li>
+ </ul>
+</form>
+
+</body>
+</html>
---
+++ t1/src/result.php
@@ -0,0 +1,17 @@
+<?php
+$length = $_POST["result-q1"];
+$a = "hi";
+$b = 0;
+?>
+
+
+<html>
+<body>
+<script>
+alert("<?php echo "hi {$length} ss" ?>")
+window.location.href='index.php';
+</script>
+
+
+</body>
+</html>
